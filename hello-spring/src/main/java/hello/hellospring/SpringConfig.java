6b0e214378c58eeff6ea1252977cd179189d9725
kwhyo <rkgmlgygml@naver.com>
2021-01-26 23:15:15 +0900

    AOP 강의 실습

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,13 @@ public class SpringConfig {
     public MemberService memberService(){
         return new MemberService(memberRepository);
     }
+
+    //aop는 스프링빈에 직접 등록해서 쓰는게 좋음!
+    //@Bean
+    //public TimeTraceAop timeTraceAop(){
+    //    return new TimeTraceAop();
+    //}
+
  //   @Bean
    // public MemberRepository memberRepository(){
         //return new MemoryMemberRepository();
